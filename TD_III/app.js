49f6c12ca45ade8e4c3ec8fa1149c707a2f21d6c
Ivann <ivann.galic@edu.itescia.fr>
2020-02-04 22:45:02 +0100

    tour de jeu ok

---
+++ TD_III/app.js
@@ -0,0 +1,66 @@
+let myTable = ["rock", "scissors", "paper"];
+
+function playerRock() {
+  document.getElementById("player-image").innerHTML='<img src="imgs/hand-rock-solid.svg" alt="paper">';
+
+  let iaRandom = random(0,2);
+  let iaChoice = myTable[iaRandom];
+
+  if(iaChoice === "rock") {
+    document.getElementById("ia-image").innerHTML='<img src="imgs/hand-rock-solid.svg" alt="rock">';
+    document.getElementById("result-text").innerHTML ="Egalité !";
+  }
+  if(iaChoice === "scissors") {
+    document.getElementById("ia-image").innerHTML ='<img src="imgs/hand-scissors-solid.svg" alt="scissors">';
+    document.getElementById("result-text").innerHTML ="Gagné !";
+  }
+  if(iaChoice === "paper") {
+    document.getElementById("ia-image").innerHTML ='<img src="imgs/hand-paper-solid.svg" alt="paper">';
+    document.getElementById("result-text").innerHTML ="Perdu !";
+  }
+}
+
+function playerScissors() {
+  document.getElementById("player-image").innerHTML='<img src="imgs/hand-scissors-solid.svg" alt="scissors">';
+
+  let iaRandom = random(0,2);
+  let iaChoice = myTable[iaRandom];
+
+  if(iaChoice === "scissors") {
+    document.getElementById("ia-image").innerHTML ='<img src="imgs/hand-scissors-solid.svg" alt="scissors">';
+    document.getElementById("result-text").innerHTML ="Egalité !";
+  }
+  if(iaChoice === "paper") {
+    document.getElementById("ia-image").innerHTML ='<img src="imgs/hand-paper-solid.svg" alt="paper">';
+    document.getElementById("result-text").innerHTML ="Gagné !";
+  }
+  if(iaChoice === "rock") {
+    document.getElementById("ia-image").innerHTML='<img src="imgs/hand-rock-solid.svg" alt="rock">';
+    document.getElementById("result-text").innerHTML ="Perdu !";
+  }
+}
+
+function playerPaper() {
+  document.getElementById("player-image").innerHTML='<img src="imgs/hand-paper-solid.svg" alt="paper">';
+
+  let iaRandom = random(0,2);
+  let iaChoice = myTable[iaRandom];
+
+  if(iaChoice === "paper") {
+    document.getElementById("ia-image").innerHTML ='<img src="imgs/hand-paper-solid.svg" alt="paper">';
+    document.getElementById("result-text").innerHTML ="Egalité !";
+  }
+  if(iaChoice === "rock") {
+    document.getElementById("ia-image").innerHTML ='<img src="imgs/hand-rock-solid.svg" alt="rock">>';
+    document.getElementById("result-text").innerHTML ="Gagné !";
+  }
+  if(iaChoice === "rock") {
+    document.getElementById("ia-image").innerHTML='<img src="imgs/hand-scissors-solid.svg" alt="scissors">';
+    document.getElementById("result-text").innerHTML ="Perdu !";
+  }
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
